6bbb229e6e6608581849df43520ceed67dfcfc91
Edward Castaneda <aeghin7@gmail.com>
2023-12-26 17:52:47 -0600

    image are created via api request, and are displaying properly

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Heading } from "@/components/Heading";
-import { Image } from "lucide-react";
+import { Download, ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { amountOptions, formSchema, resolutionOptions } from "./constants";
@@ -17,7 +17,9 @@ import { useRouter } from "next/navigation";
 
 import { Empty } from "@/components/Empty";
 import { Loader } from "@/components/Loader";
+import { Card, CardFooter } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
@@ -43,9 +45,9 @@ const ImagePage = () => {
         try {
             setImages([]);
             const response = await axios.post("/api/image", values);
-
+            // console.log(response);
             const urls = response.data.map((image: { url: string }) => image.url);
-
+            // console.log(urls);
             setImages(urls);
 
             form.reset();
@@ -62,7 +64,7 @@ const ImagePage = () => {
             <Heading
                 title="Image Generation"
                 description="Turn your prompt into an image!"
-                icon={Image}
+                icon={ImageIcon}
                 iconColor="text-pink-700"
                 bgColor="bg-pink-700/10"
             />
@@ -160,8 +162,30 @@ const ImagePage = () => {
                     {images.length === 0 && !isLoading && (
                         <Empty label="No Images. Start by prompting!" />
                     )}
-                    <div>
-                        images will be here
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+                        {images.map((src) => (
+                            <Card
+                                key={src}
+                                className="rounded-lg overflow-hidden"
+                            >
+                                <div className="relative aspect-square">
+                                    <Image
+                                        alt="image"
+                                        fill
+                                        src={src}
+                                    />
+                                </div>
+                                <CardFooter className="p-2">
+                                    <Button
+                                        onClick={() => window.open(src)}
+                                        variant='secondary'
+                                        className="w-full">
+                                        <Download className="h-4 w-4 mr-2" />
+                                        Download
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        ))}
                     </div>
                 </div>
             </div>
